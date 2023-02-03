c3c7035653e3c2829ed73a73f5b5996c48e98cfd
colebmiller <89547632+cmillz32@users.noreply.github.com>
2023-02-03 12:35:47 -0600

    Changed GitLab to use helper class

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -2,7 +2,8 @@ package edu.unl.raikes.git;
 
 public class GitLab {
     public static void main(String[] args) {
-        System.out.println("This is a lesson on git");
+    	GitLabHelper helper = new GitLabHelper("Cole", "Emma");
+    	helper.PrintWelcomeMessage();
     }
     
 }
